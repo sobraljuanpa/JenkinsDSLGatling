37546b56ac855542d80d380eb203504d20a340c2
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2020-05-12 10:21:57 -0300

    Se agrega ejecucion de prueba de Gatling al job

--- jobs/runGatlingTests.groovy
+++ jobs/runGatlingTests.groovy
@@ -1,7 +1,18 @@
 job('RunGatlingTests') {
-    steps {
-        steps {
-            shell('echo Running gatling tests')
+    triggers {
+        scm('* * * * *')
+    }
+
+    scm {
+        git {
+            remote {
+                github('sobraljuanpa/JenkinsGatlingDSL', 'https')
+                credentials('githubCredentials')
+            }
         }
+    }
+
+    steps {
+        shell('cd POC && mvn clean gatling:test -Dgatling.simulationClass=test.LibraryAPI')
     }
 }
